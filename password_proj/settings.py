1682c7749825d80c4c5e91e95f0717c38fbe33f2
scientistAdil <warafanalakaziqrak@gmail.com>
2021-08-20 21:45:55 +0530

    Fixed staticroot

--- password_proj/settings.py
+++ password_proj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5^3+f*$i@*zrh9paf5$hjl8)$8f_lq5stmo#f1_qzk39(2$%@r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'generetors.herokuapp.com']
+ALLOWED_HOSTS = [ 'localhost','127.0.0.1','generetors.herokuapp.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
